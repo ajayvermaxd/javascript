6babcb79a660e728d3e71e90350a0080bec416ff
ajayvermaxd <studajay3@gmail.com>
2023-11-02 11:50:19 +0530

    calculation using function

---
+++ calculation.js
@@ -0,0 +1,29 @@
+
+    function addition(a, b) {
+        result = a + b;
+        console.log(result);
+        // return result;
+      }
+    addition(5, 4);
+
+
+    function multiple(a, b) {
+        result = a * b;
+        console.log(result);
+        // return result;
+      }
+    multiple(5, 4);
+
+    function divide(a, b) {
+        result = a / b;
+        console.log(result);
+        // return result;
+      }
+    divide(5, 4);
+
+    function minus(a, b) {
+        result = a - b;
+        console.log(result);
+        // return result;
+      }
+    minus(5, 4);
