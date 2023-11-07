3792975219f15781eb9700cf89c24c95bea5ea40
ajayvermaxd <studajay3@gmail.com>
2023-11-07 14:58:39 +0530

    graded assignment on js and object

---
+++ assignment.js
@@ -0,0 +1,22 @@
+// Creating an Array: Define an array that stores product information for an e-commerce application. 
+// Each element in the array should be a string containing product details, such as "Product Name - Price - Category - Availability."
+let arr = [  "Product A - 1000 - Category 1 - In Stock",
+                "Product B - 20000 - Category 2 - In Stock",
+                "Product C - 15000 - Category 1 - Out of Stock",
+                "Product D - 250- Category 2 - In Stock",]
+
+// 2. Access and Display: Write code to access and display the details of a specific product from the array.
+//  This demonstrates how arrays allow you to retrieve and display information efficiently.
+let i = userInput = prompt("Enter the index:");
+console.log(arr[i]);
+
+// 3. Adding Products: Add new products to the array by pushing new strings. 
+// Show how arrays are dynamic and how products can be inserted into the array as new items become available.
+
+arr.push["Product E - 350- Category 1 - Out of Stock"];
+
+// 4. Removing Products: Remove discontinued products from the array using the `splice()` method.
+//  This illustrates how arrays help in managing changes in the product catalog.
+
+
+
